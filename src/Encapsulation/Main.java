f9a41de338fcc22c99c4426056b2f78f6b459589
SrinivasReddy Pakala <p.sri.rdy@gmail.com>
2022-07-26 07:58:28 +0530

    Adding some methods

---
+++ src/Encapsulation/Main.java
@@ -0,0 +1,9 @@
+package Encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+        Encapsulation obj= new Encapsulation();
+        obj.setName("Srinivas");
+        System.out.println(obj.getName());
+    }
+}
